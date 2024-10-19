f0336113d367f6c38de4f7b26918c4512d35379e
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-19 13:40:51 -0500

    add banner functionality

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from 'react-router-dom'
 import './Banner.css'
+import Button from '@mui/material/Button';
+
 
 export function Banner () { 
+  const navigate = useNavigate();
   return (
     <div className='banner'>
-      yo 
+      <div className='banner-info'>
+        <p className='text'>Explore the World, One Adventure at a Time!</p>
+        <Button className='cp' onClick ={ () => (navigate('/create'))}>Create Post</Button>
+      </div>
     </div>
   )
 }
